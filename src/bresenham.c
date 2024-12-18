b22c9cd0f5fec17615f38a9ea6cb031446f6881b
Yves Gille <ygille@student.42lyon.fr>
2024-12-18 16:42:25 +0100

    Mise à la norme de bresenham

--- src/bresenham.c
+++ src/bresenham.c
@@ -6,19 +6,20 @@
 /*   By: ygille <ygille@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 17:28:06 by ygille            #+#    #+#             */
-/*   Updated: 2024/12/17 18:48:52 by ygille           ###   ########.fr       */
+/*   Updated: 2024/12/18 13:51:07 by ygille           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 
+static void			b_calc(int *err, int dm, int *a, int sa);
 static t_bresenham	init_bresenham(t_curve curve);
 static int			max(int a, int b);
 
 /*
 ** Bresenham's line algorithm
 */
-void bresenham(t_mlx *mlx, t_curve curve)
+void	bresenham(t_mlx *mlx, t_curve curve)
 {
 	t_bresenham	b;
 	int			x;
@@ -32,29 +33,24 @@ void bresenham(t_mlx *mlx, t_curve curve)
 	while (1)
 	{
 		verif_pos(mlx, x, y, z);
-		if (x == curve.ex && y == curve.ey && z == curve.ez) 
-			break;
+		if (x == curve.ex && y == curve.ey && z == curve.ez)
+			break ;
+		b.err1 -= b.dx;
+		b.err2 -= b.dy;
+		b.err3 -= b.dz;
+		if (b.err1 < 0)
+			b_calc(&b.err1, b.dm, &x, b.sx);
+		if (b.err2 < 0)
+			b_calc(&b.err2, b.dm, &y, b.sy);
+		if (b.err3 < 0)
+			b_calc(&b.err3, b.dm, &z, b.sz);
+	}
+}
 
-		err1 -= dx;
-		err2 -= dy;
-		err3 -= dz;
-
-		if (err1 < 0)
-		{
-			err1 += dm;
-			x += sx;
-        }
-        if (err2 < 0)
-        {
-            err2 += dm;
-            y += sy;
-        }
-        if (err3 < 0)
-        {
-            err3 += dm;
-            z += sz;
-        }
-    }
+static void	b_calc(int *err, int dm, int *a, int sa)
+{
+	*err += dm;
+	*a += sa;
 }
 
 static t_bresenham	init_bresenham(t_curve curve)
@@ -76,10 +72,10 @@ static t_bresenham	init_bresenham(t_curve curve)
 		b.sz = 1;
 	else
 		b.sz = -1;
-	b.dm = max(dx, max(dy, dz));
-	b.err1 = dm / 2;
-	b.err2 = dm / 2;
-	b.err3 = dm / 2;
+	b.dm = max(b.dx, max(b.dy, b.dz));
+	b.err1 = b.dm / 2;
+	b.err2 = b.dm / 2;
+	b.err3 = b.dm / 2;
 	return (b);
 }
 
